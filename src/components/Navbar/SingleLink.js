42fc258f41aae1bab57812e709aef2d80338cf68
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-10 22:51:44 +0530

    link element

---
+++ src/components/Navbar/SingleLink.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './SingleLink.module.css';
+
+const SingleLink = ({ title }) => {
+	return (
+		<a href="#" className={classes.link}>
+			{title}
+		</a>
+	);
+};
+
+export default SingleLink;
