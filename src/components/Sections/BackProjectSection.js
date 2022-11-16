b1b4e4f221ec19471a3291b67354e687e92c6505
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-16 22:22:19 +0530

    paragraph and button component imported

--- src/components/Sections/BackProjectSection.js
+++ src/components/Sections/BackProjectSection.js
@@ -3,6 +3,8 @@ import Card from '../Card/Card';
 import Title from '../Texts/Title';
 import classes from './BackProjectSection.module.css';
 import MasterCraftLogo from '../../assets/logo-mastercraft.svg';
+import Paragraph from '../Texts/Paragraph';
+import Button from '../Button/Button';
 const BackProjectSection = () => {
   return (
     <Card primary={true}>
@@ -14,6 +16,17 @@ const BackProjectSection = () => {
         />
         <div className={classes['title-box']}>
           <Title primary={true} content="Mastercraft Bamboo Monitor Riser" />
+        </div>
+        <div className={classes.description}>
+          <Paragraph
+            content="A beautifully handcrafted monitor stand to reduce neck and eye strain."
+            type="primary"
+          />
+        </div>
+        <div className={classes['button-section']}>
+          <Button type="primary" size="large">
+            Back this project
+          </Button>
         </div>
       </section>
     </Card>
