e02c7948b51b01474a813398f629d239c0e10a9c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-22 19:17:15 +0530

    backed details section progress

--- src/components/Card/MainCard.js
+++ src/components/Card/MainCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import classes from './MainCard.module.css';
 import BackProjectSection from '../Sections/BackProjectSection';
+import BackedDetailsSection from '../Sections/BackedDetailsSection';
 const MainCard = ({ children }) => {
   return (
     <div className={classes['main-card']}>
       <section>
-        <BackProjectSection />
+        <div className={classes['backproject-section']}>
+          <BackProjectSection />
+        </div>
+        <div className={classes['backed-details-section']}>
+          <BackedDetailsSection />
+        </div>
       </section>
     </div>
   );
