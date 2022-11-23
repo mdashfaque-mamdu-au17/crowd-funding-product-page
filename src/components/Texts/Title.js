98ad842f4283517f7c3172431ff23ea2bbb19a46
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-23 20:22:00 +0530

    secondary title added

--- src/components/Texts/Title.js
+++ src/components/Texts/Title.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import classes from './Title.module.css';
 
-const Title = ({ primary, children }) => {
-  if (primary) {
+const Title = ({ primary, secondary, size, children }) => {
+  if (secondary) {
+    return <h3 className={classes['secondary-heading']}>{children}</h3>;
+  }
+  if (primary && !size) {
     return <h1 className={classes['primary-heading']}>{children}</h1>;
   }
-  if (!primary) {
+  if (!primary && !size) {
     return <h1 className={classes['primary-digits']}>{children}</h1>;
+  }
+  if (primary && size === 'small') {
+    return <h3 className={classes['primary-small-heading']}>{children}</h3>;
   }
 };
 
