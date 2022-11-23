6486a006e11bd50b011a90904be6af6fdfc3756f
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-23 20:21:26 +0530

    text color change for amount

--- src/components/Texts/Paragraph.js
+++ src/components/Texts/Paragraph.js
@@ -4,7 +4,14 @@ import classes from './Paragraph.module.css';
 const Paragraph = ({ type, content }) => {
   const classType =
     type === 'primary' ? classes['para-primary'] : classes['para-secondary'];
-  return <p className={classType}>{content}</p>;
+  if (type === 'primary' || type === 'secondary') {
+    return <p className={classType}>{content}</p>;
+  }
+  if (type === 'primaryAmount') {
+    return (
+      <p className={classes['primary-amount']}>Pledge ${content} or more</p>
+    );
+  }
 };
 
 export default Paragraph;
