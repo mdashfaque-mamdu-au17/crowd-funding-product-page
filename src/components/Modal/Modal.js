3d483d51130463f17810f758a198cfb48b8e4c00
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-24 22:31:32 +0530

    modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import classNames from 'classnames';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import classes from './Modal.module.css';
+
+const Backdrop = () => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = ({ type, children }) => {
+  let modalType;
+  if (type === 'select-pledge') {
+    modalType = 'select-pledge';
+  }
+  if (type === 'welcome-modal') {
+    modalType = 'welcome-modal';
+  }
+  if (type === 'menu-modal') {
+    modalType = 'menu-modal';
+  }
+  return (
+    <div className={classNames(classes.modal, classes[modalType])}>
+      {children}
+    </div>
+  );
+};
+
+const portalElement = document.getElementById('overlay');
+const Modal = ({ type, children }) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay type={type}>{children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
