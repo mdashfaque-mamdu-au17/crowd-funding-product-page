8bde53b2611616783239e014ee6adfa34d404ef4
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-24 22:31:06 +0530

    select option dropdown added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import MainCard from './components/Card/MainCard';
+import SelectOptionModal from './components/Modal/SelectOptionModal';
 import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div>
       <Navbar />
       <MainCard />
+      <SelectOptionModal />
     </div>
   );
 }
---
+++ src/components/Modal/SelectOptionModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Modal from './Modal';
+import classes from './SelectOptionModal.module.css';
+const SelectOptionModal = () => {
+  return <Modal type="menu-modal">Hello world</Modal>;
+};
+
+export default SelectOptionModal;
+
+// type="select-pledge", type="welcome-modal", type="menu-modal"
