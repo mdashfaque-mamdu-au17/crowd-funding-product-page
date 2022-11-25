d83e9c590d264b328b9f3e84400390d885850368
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-25 17:39:24 +0530

    modal state and scroll behaviour added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useState } from 'react';
 import './App.css';
 import MainCard from './components/Card/MainCard';
 import SelectOptionModal from './components/Modal/SelectOptionModal';
 import Navbar from './components/Navbar/Navbar';
+import { enableScroll, disableScroll } from './utils/utils';
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => {
+    setIsModalOpen(false);
+    enableScroll();
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    disableScroll();
+  };
+
   return (
     <div>
       <Navbar />
-      <MainCard />
-      <SelectOptionModal />
+      <MainCard onOpen={openModal} />
+      {isModalOpen && <SelectOptionModal onClose={closeModal} />}
     </div>
   );
 }
