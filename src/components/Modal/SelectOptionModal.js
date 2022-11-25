b562200460fccd9f8bee0c56e2e451a3e96f89f5
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-25 17:43:30 +0530

    select option progress

--- src/components/Modal/SelectOptionModal.js
+++ src/components/Modal/SelectOptionModal.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from './Modal';
+import { CloseModalIcon } from '../Button/CloseModalIcon';
 import classes from './SelectOptionModal.module.css';
-const SelectOptionModal = () => {
-  return <Modal type="menu-modal">Hello world</Modal>;
+
+const SelectOptionModal = ({ onClose }) => {
+  return (
+    <Modal type="select-pledge">
+      <section className={classes['select-pledge-main']}>
+        <div className={classes['select-pledge-heading']}>
+          <h3>Back this project</h3>
+          <button className={classes['close-btn']} onClick={onClose}>
+            <CloseModalIcon />
+          </button>
+        </div>
+      </section>
+    </Modal>
+  );
 };
 
 export default SelectOptionModal;
