4b7e117e6b5c5f792f16e2d70fb580bcd25c6687
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-11-25 17:40:08 +0530

    error due to fill-rule is corrected

--- src/components/Button/BookMarkIcon.js
+++ src/components/Button/BookMarkIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BookMarkIcon = ({ fillColor = '#2F2F2F' }) => {
   return (
     <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg">
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <circle fill={fillColor} cx="28" cy="28" r="28" />
         <path fill="#B1B1B1" d="M23 19v18l5-5.058L33 37V19z" />
       </g>
