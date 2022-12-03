733df2a5fedfbe5ce375ef84b4c9474ecb380790
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-03 21:35:48 +0530

    select box progress

--- src/components/Card/SelectBox.js
+++ src/components/Card/SelectBox.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import Paragraph from '../Texts/Paragraph';
 import classes from './SelectBox.module.css';
 const SelectBox = () => {
   const [active, setActive] = useState(false);
@@ -21,8 +22,18 @@ const SelectBox = () => {
       <div>
         <label htmlFor="" className={classes.label}>
           <input type="radio" readOnly checked={active} />
-          shit
+          <div className={classes['label-info']}>
+            <h4>Pledge with no reward</h4>
+            <p>Pledge $25 or more</p>
+          </div>
         </label>
+      </div>
+
+      <div className={classes['para-container']}>
+        <Paragraph
+          type="primary"
+          content="Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email."
+        />
       </div>
     </div>
   );
