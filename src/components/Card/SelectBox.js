545a0fd04a4c41396827d280b675a8de84c41060
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-05 21:53:06 +0530

    amount input field and change in logic on how to show it completed

--- src/components/Card/SelectBox.js
+++ src/components/Card/SelectBox.js
@@ -1,40 +1,95 @@
 import classNames from 'classnames';
 import React, { useState } from 'react';
+import { useDetectClickOutside } from 'react-detect-click-outside';
 import Paragraph from '../Texts/Paragraph';
+import Button from '../Button/Button';
 import classes from './SelectBox.module.css';
-const SelectBox = () => {
+const SelectBox = (props) => {
+  const {
+    withReward,
+    rewardDetails,
+    minReward,
+    rewardLeft,
+    pledgeName,
+    isDisabled,
+  } = props;
   const [active, setActive] = useState(false);
+  const changeActiveState = () => {
+    setActive(false);
+  };
+  const ref = useDetectClickOutside({ onTriggered: changeActiveState });
   const focusHandler = () => {
     setActive(true);
   };
 
-  const handlerBlur = () => {
-    setActive(false);
-  };
-
   return (
     <div
-      className={classNames(classes.card, active && classes['card-active'])}
+      ref={ref}
+      className={classNames(
+        classes.card,
+        active && !isDisabled && classes['card-active'],
+        isDisabled && classes['card-disabled']
+      )}
       onFocus={focusHandler}
-      onBlur={handlerBlur}
       tabIndex="-1"
     >
-      <div>
+      <div className={classes['pledge-details']}>
         <label htmlFor="" className={classes.label}>
-          <input type="radio" readOnly checked={active} />
+          <input type="radio" readOnly checked={active && !isDisabled} />
           <div className={classes['label-info']}>
-            <h4>Pledge with no reward</h4>
-            <p>Pledge $25 or more</p>
+            <h4>{pledgeName}</h4>
+            {withReward && <p>Pledge ${minReward} or more</p>}
           </div>
         </label>
+
+        <div className={classes['amount-left-web']}>
+          {withReward && (
+            <p>
+              {rewardLeft} <span>left</span>
+            </p>
+          )}
+        </div>
       </div>
 
       <div className={classes['para-container']}>
-        <Paragraph
-          type="primary"
-          content="Choose to support us without a reward if you simply believe in our project. As a backer, you will be signed up to receive product updates via email."
-        />
+        <Paragraph type="primary" content={rewardDetails} />
       </div>
+
+      <div className={classes['amount-left-section']}>
+        <p>
+          {rewardLeft} <span>left</span>
+        </p>
+      </div>
+
+      {active && withReward && (
+        <div className={classNames(classes['amount-input-section'])}>
+          <div className={classes['amount-input-section-container']}>
+            <div className={classes['amount-section-pledge-label']}>
+              <Paragraph type="primary" content="Enter your pledge" />
+            </div>
+            <div className={classes['amount-actions-container']}>
+              <div className={classes['amount-input']}>
+                <span>$</span>
+                <span className={classes['input-right-border']}></span>
+                <input
+                  type="text"
+                  id="price"
+                  placeholder={minReward}
+                  className={classes['input']}
+                  maxLength="3"
+                  pattern="[0-9]+"
+                />
+              </div>
+
+              <div>
+                <Button type="primary" size="small" isDisabled={isDisabled}>
+                  Continue
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
