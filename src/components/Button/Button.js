6b1b79b7f98db17c81e8a230bb96ea4257aeed7c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-05 21:52:19 +0530

    small button added

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -5,6 +5,20 @@ import classes from './Button.module.css';
 const Button = ({ type, size, children, isDisabled, onClick }) => {
   const btnSizeClass =
     size === 'large' ? classes['btn-large'] : classes['btn-small'];
+
+  if (type === 'primary' && size === 'small') {
+    return (
+      <button
+        className={classNames(
+          classes['primary-btn'],
+          classes['btn-small'],
+          isDisabled && classes['btn-small-disabled']
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
   if (type === 'primary' && size === 'medium') {
     return (
       <button
