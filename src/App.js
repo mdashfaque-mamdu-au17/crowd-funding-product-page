f111513176f1be6d95a160506cd6612d247c36d3
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-06 22:06:47 +0530

    thank you modal added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './App.css';
 import MainCard from './components/Card/MainCard';
 import SelectOptionModal from './components/Modal/SelectOptionModal';
+import ThankYou from './components/Modal/ThankYou';
 import Navbar from './components/Navbar/Navbar';
 import { useGlobalContext } from './store/context';
 import {} from './utils/utils';
 
 function App() {
-  const { openSelectBox } = useGlobalContext();
-
+  const { openSelectBox, thankYou } = useGlobalContext();
   return (
     <div>
       <Navbar />
       <MainCard />
       {openSelectBox && <SelectOptionModal />}
+      {thankYou && <ThankYou />}
     </div>
   );
 }
