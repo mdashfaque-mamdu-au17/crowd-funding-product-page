74e4a789a9e114e33060e9db100820a28f64225b
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-06 19:32:07 +0530

    moved states to context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import MainCard from './components/Card/MainCard';
 import SelectOptionModal from './components/Modal/SelectOptionModal';
 import Navbar from './components/Navbar/Navbar';
-import { enableScroll, disableScroll } from './utils/utils';
+import { useGlobalContext } from './store/context';
+import {} from './utils/utils';
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const closeModal = () => {
-    setIsModalOpen(false);
-    enableScroll();
-  };
-
-  const openModal = () => {
-    setIsModalOpen(true);
-    disableScroll();
-  };
+  const { openSelectBox } = useGlobalContext();
 
   return (
     <div>
       <Navbar />
-      <MainCard onOpen={openModal} />
-      {isModalOpen && <SelectOptionModal onClose={closeModal} />}
+      <MainCard />
+      {openSelectBox && <SelectOptionModal />}
     </div>
   );
 }
