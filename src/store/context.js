d7bffa84e703f5f59c088c9ba697c9797941f390
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-12-06 19:35:11 +0530

    logic update in context

--- src/store/context.js
+++ src/store/context.js
@@ -1,14 +1,44 @@
-import React, { useContext } from 'react';
-
-const initialState = {
-  isModalOpen: true,
-};
-
+import React, { useState, useContext } from 'react';
+import { enableScroll, disableScroll } from '../utils/utils';
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+  const [openSelectBox, setOpenSelectBox] = useState(false);
+  const [backedTotal, setBackedTotal] = useState(89194);
+  const [numberOfBackers, setNumberOfBackers] = useState(5007);
+  const closeSelectBoxModal = () => {
+    setOpenSelectBox(false);
+    enableScroll();
+  };
+
+  const openSelectBoxModal = () => {
+    setOpenSelectBox(true);
+    disableScroll();
+  };
+
+  const updateBackedTotal = (amount) => {
+    if (backedTotal > 100000) {
+      return;
+    }
+    setBackedTotal((prev) => prev + Number(amount));
+  };
+
+  const updateNumberOfBackers = () => {
+    setNumberOfBackers((prev) => prev + 1);
+  };
+
   return (
-    <AppContext.Provider value={{ ...initialState }}>
+    <AppContext.Provider
+      value={{
+        openSelectBox,
+        backedTotal,
+        closeSelectBoxModal,
+        openSelectBoxModal,
+        updateBackedTotal,
+        numberOfBackers,
+        updateNumberOfBackers,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
